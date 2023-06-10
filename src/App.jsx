45d183510184e4f6b356126898e6634fb19bc4b5
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-06-10 16:24:23 +0500

    dix

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Todo from "./Todo";
+
+const App = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-4">Todo App</h1>
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
